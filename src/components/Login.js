ce6775e543da8ef856e4222a6469202aa2a99f5f
AbhishekWMG <ak89471@gmail.com>
2025-09-12 18:00:41 +0530

    sign in/up form

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,9 +18,10 @@ const Login = () => {
             <form className='absolute w-3/12 p-12 bg-black my-24 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
                 <h1 className='font-bold text-3xl py-4'>{isSignInForm ? 'Sign In' : "Sign Up"}</h1>
                 <input type="text" placeholder='Email or mobile number' className='p-4 my-4 w-full bg-transparent border' />
-                <input type="text" placeholder='Password' className='p-4 my-4 w-full bg-transparent border' />
-                <button className='p-4 my-4 bg-red-700 w-full rounded-lg'>Sign In</button>
-                <p className='py-4 cursor' onClick={toggleSignInForm}>New to Netflix? Sign Up Now!</p>
+                {!isSignInForm && <input type="text" placeholder='Enter Full Name' className='p-4 my-4 w-full bg-transparent border' />}
+                <input type="password" placeholder='Password' className='p-4 my-4 w-full bg-transparent border' />
+                <button className='p-4 my-4 bg-red-700 w-full rounded-lg'>{isSignInForm ? 'Sign In' : "Sign Up"}</button>
+                <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? 'New to Netflix? Sign Up Now!' : "Already Registered? Sign In Here"}</p>
             </form>
         </div>
     )
